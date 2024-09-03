62466c5cded0b5f76cefa8f950bdfa855218e7a6
MarsGotBars <marcinm@live.nl>
2024-09-03 15:11:46 +0200

    Stacking prep
    Figure and article are ready to be stacked, will most likely do so with pos: absolute or grid 1fr trick

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import "./App.css";
 import Card from "./components/card/Card";
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,22 +3,24 @@ import "./card.css";
 export default function Card() {
   return (
     <main>
-      <figure>
-        <figcaption>
-          {/* find image */}
-          <h1>Marcin</h1>
-        </figcaption>
-      </figure>
-      {/* <article>
-        <div>
-          <h2>Hobby's</h2>
-          <ul>
-            <li>Coding</li>
-            <li>Testing</li>
-            <li>extra</li>
-          </ul>
-        </div>
-      </article> */}
+      <div className="container">
+        <figure>
+          <figcaption className="content">
+            {/* find image */}
+            <h1>Marcin</h1>
+          </figcaption>
+        </figure>
+        <article className="content">
+          <div>
+            <h2>Hobbies</h2>
+            <ul>
+              <li>Coding</li>
+              <li>Testing</li>
+              <li>extra</li>
+            </ul>
+          </div>
+        </article>
+      </div>
     </main>
   );
 }
