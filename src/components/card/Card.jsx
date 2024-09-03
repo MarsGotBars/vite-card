bdd6e4b7f916e02b757c591d8293b8106708e8ae
MarsGotBars <marcinm@live.nl>
2024-09-03 11:45:44 +0200

    changes
    quick

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './card.css'
+export default function Card() {
+  return (
+    <main>
+        <figure>
+            <figcaption></figcaption>
+        </figure>
+        <header>
+            <h1>card</h1>
+            <a href="">test</a>
+        </header>
+        <ul>
+            <li>text</li>
+            <li>asd</li>
+            <li>zxc</li>
+            <li>sads</li>
+        </ul>
+    </main>
+  )
+}
